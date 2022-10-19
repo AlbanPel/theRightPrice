28d8730f49691b2aad900640b6a5ec05116dcdf6
Alban Pelissier <alban@qodlab.io>
2022-10-19 18:15:39 +0200

    Categories

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String [] categories = {"Vêtement", "Voiture", "Immobilier"};
+        int cat = new Random().nextInt(0,3);
+        System.out.println(categories[cat]);
+        int price;
+        if (cat == 0) {
+           price = new Random().nextInt(5, 200);
+        } else if (cat == 1) {
+           price = new Random().nextInt(5000, 75000);
+        }else {
+           price = new Random().nextInt(90000, 500000);
+        }
+
+        System.out.println(price);
+
     }
 }
