a1459bcf01695dab938573ef35b225e58ca3e2b4
Alban Pelissier <alban@qodlab.io>
2022-10-19 18:56:34 +0200

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        String [] categories = {"Vêtement", "Voiture", "Immobilier"};
+        String [] categories = {"Vêtement de 5€ - 200€", "Voiture de 5 000€ - 75 000€", "Immobilier de 90 000€ - 500 000€"};
         int cat = new Random().nextInt(0,3);
-        System.out.println(categories[cat]);
         int price;
         if (cat == 0) {
            price = new Random().nextInt(5, 200);
@@ -14,8 +14,27 @@ public class Main {
         }else {
            price = new Random().nextInt(90000, 500000);
         }
+        int myPrice = 0;
 
-        System.out.println(price);
+        System.out.println("Entrez le juste prix ! indice => " + categories[cat]);
+        int tryAgain = 0;
+        while (price != myPrice){
+            Scanner myProposition = new Scanner(System.in);
+
+            if (price > myPrice  && myPrice !=0){
+                System.out.println("C'est plus !");
+            } else if (myPrice == 0){
+                System.out.println("Bonne chance");
+            }
+            else {
+                System.out.println("C'est moins !");
+            }
+            myPrice = myProposition.nextInt();
+            tryAgain++;
+        }
+        System.out.print("Bravo ! vous avez trouvé le juste prix : " + price + "$");
+
+        System.out.println(" en " + tryAgain + " fois");
 
     }
 }
